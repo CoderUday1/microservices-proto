f6f53a417a4d30b87a30818774fa58edf50b74d5
CoderUday1 <udaykiranbakka4code@gmail.com>
2025-08-20 23:08:47 +0530

    update protoc

--- protoc.sh
+++ protoc.sh
@@ -5,23 +5,41 @@ RELEASE_VERSION=$2
 USER_NAME=$3
 EMAIL=$4
 
+# Configure git
 git config user.name "$USER_NAME"
 git config user.email "$EMAIL"
-git fetch --all && git checkout main
 
+# Make sure we're on latest main
+git fetch origin main
+git checkout main
+git pull origin main   # keep local in sync with remote
+
+# Install required tools
+sudo apt-get update
 sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
+# Generate proto files
 protoc --go_out=./golang --go_opt=paths=source_relative \
   --go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
- ./${SERVICE_NAME}/*.proto
+  ./${SERVICE_NAME}/*.proto
+
+# Init go module for this service
 cd golang/${SERVICE_NAME}
-go mod init \
-  github.com/huseyinbabal/microservices-proto/golang/${SERVICE_NAME} || true
+go mod init github.com/huseyinbabal/microservices-proto/golang/${SERVICE_NAME} || true
 go mod tidy
 cd ../../
-git add . && git commit -am "proto update" || true
+
+# Commit changes (skip if nothing new)
+git add .
+git commit -m "proto update for ${SERVICE_NAME}" || echo "No changes to commit"
+
+# Push safely (rebase if needed)
+git pull --rebase origin main
 git push origin HEAD:main
+
+# Create / update tag
 git tag -fa golang/${SERVICE_NAME}/${RELEASE_VERSION} \
-  -m "golang/${SERVICE_NAME}/${RELEASE_VERSION}" 
-git push origin refs/tags/golang/${SERVICE_NAME}/${RELEASE_VERSION}
+  -m "golang/${SERVICE_NAME}/${RELEASE_VERSION}"
+git push origin refs/tags/golang/${SERVICE_NAME}/${RELEASE_VERSION} --force

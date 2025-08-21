ad057d7b447bcca82294ee35a16b6b30c720e5f4
CoderUday1 <udaykiranbakka4code@gmail.com>
2025-08-21 21:11:48 +0530

    update

--- protoc.sh
+++ protoc.sh
@@ -27,7 +27,9 @@ protoc --go_out=./golang --go_opt=paths=source_relative \
 
 # Init go module for this service
 cd golang/${SERVICE_NAME}
-go mod init edit -module="github.com/CoderUday/microservices-proto/golang/${SERVICE_NAME}" || true
+rm go.mod
+rm go.sum
+go mod init github.com/CoderUday1/microservices-proto/golang/${SERVICE_NAME}" || true
 go mod tidy
 cd ../../
 
